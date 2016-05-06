0a78e7f99708519dd9f8a2cac5cc14a4258e26e1
Matt Robinson <matt@technicalartisan.com>
2016-05-05 20:33:33 -0400

    house keeping

--- elemental_kinds/_formatted_string_kind.py
+++ elemental_kinds/_formatted_string_kind.py
@@ -6,16 +6,16 @@ class FormattedStringKind(StringKind):
     @staticmethod
     def process_value(value, **type_properties):
         formatter = type_properties.get('formatter')
-        formatter = str(formatter).strip() if formatter else formatter
+        formatter = str(formatter).strip().format if formatter else formatter
 
         if formatter:
             try:
-                result = formatter.format(**value)
-            except Exception:
+                result = formatter(**value)
+            except Exception as e:
                 msg = 'Value "{0}" does not conform to format "{0}".'
                 msg = msg.format(value, formatter)
 
-                raise AttributeKindValueProcessingError(msg)
+                raise AttributeKindValueProcessingError(msg, inner_error=e)
         else:
             msg = 'Invalid formatter: "{0}"'
             msg = msg.format(formatter)
